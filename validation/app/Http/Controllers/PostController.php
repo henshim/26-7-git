6d2a107e5592237e64f3ea6b510ff8dbec135c20
henshim <hieume113@gmail.com>
2021-07-26 11:16:54 +0700

    validation1

---
+++ validation/app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $validate = $request->validate(['number' => 'required|numeric']);
+        echo 'đã xác thực thành công';
+    }
+}
